0f475a23cee4a5d985180c5739ea334055cb4b29
Thale Jacobs <thale_jacobs@yahoo.com>
2026-07-23 11:38:08 -0400

    Refactored package location SavedPropertiesController and SavedPropertiesRepository

--- auction-scout-manager/src/main/java/com/oncoord/auctionscout/saved/SavedPropertiesController.java
+++ auction-scout-manager/src/main/java/com/oncoord/auctionscout/saved/SavedPropertiesController.java
@@ -5,7 +5,6 @@ import com.oncoord.auctionscout.subscriber.SubscriberRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
